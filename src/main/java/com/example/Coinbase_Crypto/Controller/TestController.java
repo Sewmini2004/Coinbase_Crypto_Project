ab8b7dd706368a52fdc727e9e5b9644726ce0249
Sewmini Premodya <sewminipremodya98@gmail.com>
2024-09-08 22:42:46 +0530

    deploy to ec2

--- src/main/java/com/example/Coinbase_Crypto/Controller/TestController.java
+++ src/main/java/com/example/Coinbase_Crypto/Controller/TestController.java
@@ -1,12 +1,14 @@
 package com.example.Coinbase_Crypto.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping
