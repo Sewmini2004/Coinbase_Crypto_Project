1754007fa5f972000e7df0690afe72e85b36725b
Sewmini Premodya <sewminipremodya98@gmail.com>
2024-09-08 18:11:35 +0530

    Initial commit

---
+++ src/main/java/com/example/Coinbase_Crypto/CoinbaseCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Coinbase_Crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinbaseCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinbaseCryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Coinbase_Crypto/CoinbaseCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Coinbase_Crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinbaseCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
