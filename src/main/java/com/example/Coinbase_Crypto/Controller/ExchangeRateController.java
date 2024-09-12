04e0f58581de83403398c8e480932e5e88e84583
Sewmini Premodya <sewminipremodya98@gmail.com>
2024-09-13 04:16:08 +0530

    Commit

---
+++ src/main/java/com/example/Coinbase_Crypto/Controller/ExchangeRateController.java
@@ -0,0 +1,18 @@
+package com.example.Coinbase_Crypto.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//Controller
+
+//Service
+
+//Dto
+
+//Entity
+
+//Repository
+// ethkot db set krnn nee db name ewa
+@RestController
+@RequestMapping("/a/exchangeRa")
+public class ExchangeRateController {
+}
